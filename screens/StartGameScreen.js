2453f197fbc7a43de94a1f4ed583bcbd2121fa71
asyrafahmad <are.syraf9764@gmail.com>
2020-04-05 02:45:16 +0800

    Configuring and Styling a TextInput - 59

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,7 +2,8 @@
  import { View, Text, StyleSheet, Button, TextInput} from 'react-native';
 
  import Card from '../components/Card';
- import Colors from '../cons tants/colors';
+ import Input from '../components/Input';
+ import Colors from '../constants/colors';
 
 const StartGameScreen = props => {
     return (
@@ -10,7 +11,14 @@ const StartGameScreen = props => {
             <Text style={styles.title}>Start a New Game!</Text>
             <Card style={styles.inputContainer}>
                 <Text>Select a Number</Text>
-                <TextInput />
+                <Input 
+                    style={styles.input}
+                    blurOnSubmit
+                    autoCapitalize = "none"
+                    autoCorrect = {false}
+                    keyboardType = "number-pad"
+                    maxLength= {2} 
+                />
                 <View style={styles.buttonContainer}>
                     <View style={styles.button}>
                         <Button title="Reset" onPress={() => {}} color={Colors.accent}/>
@@ -47,6 +55,10 @@ const StartGameScreen = props => {
     },
     button: {
         width: 100
+    },
+    input: {
+        width: 50,
+        textAlign: 'center',
     }
 });
 
